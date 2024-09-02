f6eea46996e4a8d1b82b48904b20fb383bdd57d3
clfnts <dolcielofuentes18@gmail.com>
2024-09-03 01:27:43 +0800

    dami icon

--- frontend/src/pages/shutlLoggedOut.jsx
+++ frontend/src/pages/shutlLoggedOut.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../css/Loggedout.css';
 
 const ShutlLoggedOut = () => {
   return (
-    <div>
-      This the logged out.
+    <div className="navbar">
+      <div className="logo">SHU TL</div>
+      <div className="status">Commuter - Logged out</div>
+      {/* New icon at the bottom of the navbar */}
+      <img src="../public/icon.png" alt="Navigation Icon" className="nav-icon" />
     </div>
-  )
-}
+  );
+};
 
-export default ShutlLoggedOut
+export default ShutlLoggedOut;
