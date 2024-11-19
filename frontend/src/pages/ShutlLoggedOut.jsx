6418e56ef9139fed11f1a4d0eb834e64a4c274a6
Vlyde Angeles <vldngls.0418@gmail.com>
2024-11-20 03:15:15 +0800

    modified:   frontend/src/pages/ShutlLoggedOut.jsx

--- frontend/src/pages/ShutlLoggedOut.jsx
+++ frontend/src/pages/ShutlLoggedOut.jsx
@@ -87,7 +87,7 @@ const ShutlLoggedOut = () => {
       </div>
 
       <div className="ShutlLoggedOut-navbar">
-        <div className="ShutlLoggedOut-logo">SHU TL.</div>
+        <div className="ShutlLoggedOut-logo">SHATOOOOL</div>
         <div className="ShutlLoggedOut-status"></div>
         <div
           className="ShutlLoggedOut-icon-container"
